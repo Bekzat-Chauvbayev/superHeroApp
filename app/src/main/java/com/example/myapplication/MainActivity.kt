0190bc682bc898e58e34131b81416da681131ea2
Bekzat-Chauvbayev <kazna51k@gmail.com>
2023-10-27 20:16:07 +0600

    images and strings

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -23,98 +23,34 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.ComposeQuadrantTheme
+import com.example.myapplication.ui.theme.ClickBehavior
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeQuadrantTheme {
+            ClickBehavior {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ComposeQuadrantApp()
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun ComposeQuadrantApp() {
 
-    Column (Modifier.fillMaxWidth()) {
-        Row(Modifier.weight(1f)) {
-            ComposableInfoCard(
-                title = stringResource(R.string.title),
-                description = stringResource(R.string.description),
-                backgroundColor = Color(0xFFEADDFF),
-                modifier = Modifier.weight(1f)
 
-            )
-            ComposableInfoCard(
-                title = stringResource(R.string.title),
-                description = stringResource(R.string.description),
-                backgroundColor = Color(0xFFD0BCFF),
-                modifier = Modifier.weight(1f)
 
-            )
-        }
-            Row(Modifier.weight(1f)) {
-                ComposableInfoCard(
-                    title = stringResource(R.string.title),
-                    description = stringResource(R.string.description),
-                    backgroundColor = Color(0xFFB69DF8),
-                    modifier = Modifier.weight(1f)
-
-                )
-                ComposableInfoCard(
-                    title = stringResource(R.string.title),
-                    description = stringResource(R.string.description),
-                    backgroundColor = Color(0xFFF6EDFF),
-                    modifier = Modifier.weight(1f)
-
-                )
-            }
-        }
-
-}
-
-@Composable
-fun ComposableInfoCard(
-    title:String,
-    description:String,
-    backgroundColor: Color,
-    modifier: Modifier= Modifier
-
-){
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(backgroundColor)
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Text(text = title,
-        modifier = Modifier.padding(bottom = 16.dp),
-        fontWeight = FontWeight.Bold
-        )
-        Text(
-            text=description,
-            textAlign = TextAlign.Justify
-        )
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
 fun ComposeQuadrantAppPreview() {
-    ComposeQuadrantTheme {
-        ComposeQuadrantApp()
+    ClickBehavior {
+
     }
 }
