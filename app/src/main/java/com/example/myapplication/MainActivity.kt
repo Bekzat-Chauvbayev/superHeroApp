8ff4e7d3211c3601f44d4602f1303445a8e36f24
Bekzat-Chauvbayev <kazna51k@gmail.com>
2023-10-27 21:36:03 +0600

    fun TextAndImages when statement

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,53 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.ClickBehavior
+import com.example.myapplication.ui.theme.ClickBehaviorTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ClickBehavior {
+            ClickBehaviorTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                ClickBehavior()
 
-                }
+
             }
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ClickBehavior(){
     var current by remember { mutableStateOf(1)}
@@ -70,11 +60,36 @@ fun ClickBehavior(){
                 )
             )
         }
-    ){innerPadding ->
-        Surface {
-
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.onPrimaryContainer)
+        ) {
+            when(current){
+                1-> TextAndImages(textLabel = , image = , onClick = { /*TODO*/ })
+                2-> TextAndImages(textLabel = , image = , onClick = { /*TODO*/ })
+                3-> TextAndImages(textLabel = , image = , onClick = { /*TODO*/ })
+            }
         }
     }
+
+
+}
+
+@Composable
+fun TextAndImages(
+    textLabel:String,
+    image: Painter,
+    onClick: ()-> Unit,
+    modifier: Modifier = Modifier
+){
+    Box(modifier = modifier){
+
+    }
+
+
 
 }
 
@@ -84,8 +99,11 @@ fun ClickBehavior(){
 
 @Preview(showBackground = true)
 @Composable
-fun ComposeQuadrantAppPreview() {
-    ClickBehavior {
-
+fun ClickBehaviorPreview() {
+    ClickBehaviorTheme(){
+        ClickBehavior()
     }
+
+
+
 }
