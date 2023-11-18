35bcec74ca74039120f1a78cd24ee278c05db812
Bekzat-Chauvbayev <kazna51k@gmail.com>
2023-11-18 12:11:32 +0600

    thirdPush

--- app/src/main/java/com/example/myapplication/HeroesScreen.kt
+++ app/src/main/java/com/example/myapplication/HeroesScreen.kt
@@ -1,28 +1,42 @@
 package com.example.myapplication
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.model.Hero
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun HeroesList(
     heroes: List<Hero>,
@@ -50,7 +64,6 @@ fun HeroesList(
                     hero = hero,
                     modifier = Modifier
                         .padding(horizontal = 16.dp, vertical = 8.dp)
-                        // Animate each list item to slide in vertically
                         .animateEnterExit(
                             enter = slideInVertically(
                                 animationSpec = spring(
@@ -81,8 +94,16 @@ fun HeroListItem(
                 .padding(16.dp)
                 .sizeIn(minHeight = 72.dp)
         ) {
-            Column() {
-
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = stringResource(hero.nameRes),
+                    style = MaterialTheme.typography.displaySmall)
+                Text(text = stringResource(hero.descriptionRes),
+                style = MaterialTheme.typography.bodyLarge)
+            }
+            Box(modifier = Modifier
+                .size(72.dp)
+                .clip(RoundedCornerShape(8.dp))){
+                Image(painter = painterResource(hero.imageRes), contentDescription =null, alignment = Alignment.TopCenter )
             }
         }
     }
